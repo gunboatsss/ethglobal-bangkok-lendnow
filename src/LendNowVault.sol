a37c688fcf42f64c8e5781e61290823607b24bd9
GUNBOATs <92352063+gunboatsss@users.noreply.github.com>
2024-11-16 15:28:12 +0700

    deposit function

--- src/LendNowVault.sol
+++ src/LendNowVault.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.18;
 
 import {_indexOf, _priceAt, _lpToQuoteToken} from "ajna-core/src/libraries/helpers/PoolHelper.sol";
-import "ajna-core/src/interfaces/pool/erc20/IERC20Pool.sol";
+import {IERC20Pool} from "ajna-core/src/interfaces/pool/erc20/IERC20Pool.sol";
 
 import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
 
@@ -11,7 +11,7 @@ contract LendNowVault is ERC4626 {
     uint256 currentIndex;
     IERC20Pool immutable ajnaPool;
 
-    constructor(IERC20Pool ajnaPool_, IERC20 asset_) ERC4626(asset_) {
+    constructor(IERC20Pool ajnaPool_, IERC20 asset_, string memory symbol_) ERC4626(asset_) ERC20(string.concat("Lend Now! ", symbol_), string.concat("LN!", symbol_)) {
         ajnaPool = ajnaPool_;
         currentIndex = getPriceIndex();
     }
@@ -21,9 +21,9 @@ contract LendNowVault is ERC4626 {
     }
 
     function totalAssets() public view override returns (uint256 quoteAmount) {
-        uint256 lp_ = ajnaPool.lenderInfo(currentIndex, address(this));
+        (uint256 lp_,) = ajnaPool.lenderInfo(currentIndex, address(this));
         (uint256 bucketLP_, uint256 bucketCollateral , , uint256 bucketDeposit, ) = ajnaPool.bucketInfo(currentIndex);
-        uint256 quoteAmount = _lpToQuoteToken(
+        quoteAmount = _lpToQuoteToken(
             bucketLP_,
             bucketCollateral,
             bucketDeposit,
@@ -33,5 +33,22 @@ contract LendNowVault is ERC4626 {
         if (_underlyingDecimals < 18) {
             quoteAmount = quoteAmount / 10 ** (18 - _underlyingDecimals);
         }
+    }
+
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
+        super._deposit(caller, receiver, assets, shares);
+        _asset.approve(address(ajnaPool), assets);
+        ajnaPool.addQuoteToken(assets, getPriceIndex(), type(uint256).max);
+    }
+    
+    function _withdraw(
+        address caller,
+        address receiver,
+        address owner,
+        uint256 assets,
+        uint256 shares
+    ) internal override {
+        ajnaPool.removeQuoteToken(assets, currentIndex);
+        super._withdraw(caller, receiver, owner, assets, shares);
     }
 }
--- test/LendNowVault.t.sol
+++ test/LendNowVault.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.18;
 import {Test, console2} from "forge-std/Test.sol";
 import {LendNowVault} from "src/LendNowVault.sol";
 import {ERC20Mock} from "openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
+import {IERC20Pool} from "ajna-core/src/interfaces/pool/erc20/IERC20Pool.sol";
 import {ERC20PoolFactory} from "ajna-core/src/ERC20PoolFactory.sol";
 
 contract LendNowVaultTest is Test {
@@ -12,15 +13,27 @@ contract LendNowVaultTest is Test {
     ERC20Mock collateral;
     ERC20Mock quote;
     ERC20PoolFactory ajnaPoolFactory;
-    ERC20Pool pool;
+    IERC20Pool pool;
 
     function setUp() public {
+        ajna = new ERC20Mock();
+        collateral = new ERC20Mock();
+        quote = new ERC20Mock();
         ajnaPoolFactory = new ERC20PoolFactory(address(ajna));
-        pool = ajnaPoolFactory.deployPool(collateral, quote, 0.01e18);
-        t = new LendNowVault();
+        pool = IERC20Pool(ajnaPoolFactory.deployPool(address(collateral), address(quote), 0.01e18));
+        t = new LendNowVault(pool, quote, "USDC");
     }
 
     function test_getPrice() public {
         console2.log(t.getPriceIndex());
+    }
+
+    function test_deposit() public {
+        quote.mint(address(this), 1e18);
+        quote.approve(address(t), 1e18);
+        t.deposit(1e18, address(this));
+        console2.log(t.balanceOf(address(this)));
+        (uint256 lp, ) = pool.lenderInfo(t.getPriceIndex(), address(t));
+        console2.log(lp);
     }
 }
